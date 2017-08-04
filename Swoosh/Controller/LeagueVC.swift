288aa248c4655524542f69a06818281269632aa1
Jeremy Clerico <blops2hd@gmail.com>
2017-08-04 17:17:03 +0100

    Continuing with segues

---
+++ Swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,29 @@
+//
+//  LeagueVC.swift
+//  Swoosh
+//
+//  Created by Jeremy Clerico on 04/08/2017.
+//  Copyright © 2017 Jeremy Clerico. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "SkillVCSegue", sender: self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
